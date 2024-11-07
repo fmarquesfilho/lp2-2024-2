1c2c6374c799912ac346b6b889015551bcb5e09f
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2024-11-07 09:25:02 -0300

    exercicio com genericos

---
+++ exemplos/3-generics/Generico.java
@@ -0,0 +1,10 @@
+
+public class Generico {
+    public <T> void imprimeVetor(T v[]){
+        for (T e : v){
+            System.out.println(e);
+        }
+    }
+}
+
+
---
+++ exemplos/3-generics/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+    public static void main(String[] args) {
+        Generico teste = new Generico();
+        Double arrayD[] = {1.2, 4.2,.5, 6.4};
+        teste.imprimeVetor(arrayD);
+        Integer arrayI[] = {1,2,3,4,5};
+        teste.imprimeVetor(arrayI);
+    }
+}
